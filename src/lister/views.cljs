bf824ca695e14f0cad2099c4d25455ed173aa22a
Jaakko Kittilä <jmkitt@utu.fi>
2021-10-18 12:54:00 +0300

    CSS simplified and improved

--- src/lister/views.cljs
+++ src/lister/views.cljs
@@ -30,10 +30,10 @@
              :value category
              :on-change #(re/dispatch [::events/update-new-product-category (-> % .-value)])}
             options]]]]]
-   [:div#product-buttons.margin-center
-      [:div.big-button.bluetext.blueborder.center-text
+   [:div#product-buttons.margin-center.flex-row
+      [:div.big-button.bluetext.blueborder.center-text.hoverable
        {:on-click #(re/dispatch [::events/update-new-product nil])} "Cancel"]
-      [:div.big-button.bluetext.blueborder.center-text 
+      [:div.big-button.bluetext.blueborder.center-text.hoverable 
        {:on-click #(re/dispatch [::events/save-new-product])}"Save"]]]))
 
 (defn product-item [product search-or-view]
@@ -57,22 +57,22 @@
                 nil
                 {:max-height "250px"})]
     ^{:key id}
-    [:div.product.margin-center
+    [:div.product.margin-center.flex-row
      {:style style}
      [:div.name-and-category
       [:p.product-name product-name]
       [:p.category-name category-name]]
     (if search-or-view
-      [:div.product-view-items
-       [:div.product-amount-control.bluetext.blueborder
+      [:div.flex-row
+       [:div.product-amount-control.bluetext.blueborder.center-text.hoverable
         {:on-click #(re/dispatch [::events/update-product-amount id true])} "+"]
-       [:div.product-amount.bluetext.blueborder amount]
-       [:div.product-amount-control.bluetext.blueborder
+       [:div.product-amount.bluetext.blueborder.center-text amount]
+       [:div.product-amount-control.bluetext.blueborder.center-text.hoverable
         {:on-click #(re/dispatch [::events/update-product-amount id false])} "-"]
        [:input#checked {:type "Checkbox"
                         :checked checked
                         :on-change #(re/dispatch [::events/toggle-checked id])}]]
-     [:div.product-amount-control.bluetext.blueborder
+     [:div.product-amount-control.bluetext.blueborder.center-text
            {:on-click #(re/dispatch [::events/add-product-to-list id])} "+"])]))
 
 (defn search-view []
@@ -84,8 +84,8 @@
         product-list (doall (for [product filtered-products]
                               (product-item product false)))]
     [:div#search-view
-     [:div#list-view-top.margin-center
-      [:p#back-button.bluetext.blueborder
+     [:div.list-view-top.margin-center.flex-row
+      [:p.back-button.bluetext.blueborder.center-text.hoverable
        {:on-click #(re/dispatch [::events/update-search nil])}
        "<-"]
       [:input#list-name.bluetext.blueborder
@@ -93,7 +93,7 @@
         :value search
         :on-change #(re/dispatch [::events/update-search (-> % .-target .-value)])}]]
      [:div#product-list.margin-center product-list]
-     [:div#new-product-button.bluetext.blueborder.margin-center
+     [:div#new-product-button.bluetext.blueborder.margin-center.center-text.hoverable
       {:on-click #(re/dispatch [::events/init-new-product])}
       "Add new product"]]))
 
@@ -107,8 +107,8 @@
         product-list (doall (for [product (vals (:products active-list-contents))]
                               (product-item product true)))]
     [:div#list-view
-     [:div#list-view-top.margin-center
-      [:p#back-button.bluetext.blueborder
+     [:div.list-view-top.margin-center.flex-row
+      [:p.back-button.bluetext.blueborder.center-text.hoverable
        {:on-click #(re/dispatch [::events/set-active-list nil])}
        "<-"]
       [:input#list-name.bluetext.blueborder
@@ -116,17 +116,17 @@
         :value (:name active-list-contents)
         :on-change #(re/dispatch [::events/update-list-name (-> % .-target .-value)])}]]
      [:div#product-list.margin-center product-list]
-     [:div#list-controls 
-      [:div#pin-button.bluetext.blueborder
+     [:div#list-controls.flex-row
+      [:div#pin-button.bluetext.blueborder.center-text.hoverable
        {:on-click #(re/dispatch [::events/toggle-pinned (:id active-list-contents)])}
        pin-text]
-      [:div.plus.bluetext.blueborder {:on-click #(re/dispatch [::events/update-search ""])} "+"]]]))
+      [:div.plus.bluetext.blueborder.center-text.hoverable {:on-click #(re/dispatch [::events/update-search ""])} "+"]]]))
 
 (defn list-list [list]
   (let [return-list (doall
                      (for [l list]
                        ^{:key (:id l)}
-                       [:div.list.blueborder
+                       [:div.list.blueborder.center-text.hoverable
                         {:onClick #(re/dispatch [::events/set-active-list (:id l)])}
                         [:div.list-name (:name l)]]))]
     return-list))
@@ -140,8 +140,8 @@
     [:div#list-component.margin-center
      (when (> (count pinned-lists) 0)
        [:p.bluetext "Pinned"])
-     [:div.list-list pinned-list]
-     [:div.list-list unpinned-list]]))
+     [:div.list-list.flex-row pinned-list]
+     [:div.list-list.flex-row unpinned-list]]))
 
 (defn main-panel []
   (let [active-list @(re/subscribe [::subs/active-list])
@@ -155,7 +155,9 @@
                                [search-view]
                                [new-product-view])))]
     [:div#container.margin-center
-     [:h1#title.bluetext "Shopper"]
+     [:h1#title.bluetext.center-text "Shopper"]
      (when-not active-list
-       [:p.plus.bluetext.blueborder {:on-click #(re/dispatch [::events/init-new-list])} "+"])
+       [:div#new-product-button.bluetext.blueborder.margin-center.center-text.hoverable
+        {:on-click #(re/dispatch [::events/init-new-list])}
+        "New list"])
      active-component]))
